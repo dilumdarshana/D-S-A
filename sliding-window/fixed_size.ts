fce87bcdbe584543ae48963a81bed1ae41850722
dilumdarshana <dilum.dar@gmail.com>
2024-11-27 11:20:16 +0530

    feat:fixed window size sliding window

---
+++ sliding-window/fixed_size.ts
@@ -0,0 +1,28 @@
+/**
+ * Find maximun sum of all sub arrays of size k
+ */
+
+function findMaxSum(nums: number[], k: number): number {
+  // window size is k
+  let maxSum = 0;
+
+  // calculate initial sum
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+
+  // sliding window
+  for (let j = 1; j < nums.length; j++) {
+    sum += nums[j+k-1] - nums[j-1];
+
+    if (sum > maxSum) maxSum = sum;
+  }
+
+  return maxSum;
+}
+
+// call function
+const nums = [1, 4, 2, 10, 23, 3, 1, 0, 20];
+const k = 4;
+console.log(findMaxSum(nums, k)); // Output: 39
